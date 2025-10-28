d6a814cdf3e50d8b0ddf800b3ed8a56461b536e8
yiran1457 <3270897489@qq.com>
2025-10-28 19:46:34 +0800

    试图保留blockstate

--- src/main/java/com/sighs/oneenoughblock/utils/CoreUtils.java
+++ src/main/java/com/sighs/oneenoughblock/utils/CoreUtils.java
@@ -4,22 +4,32 @@ import com.sighs.oneenoughblock.loader.EntryCache;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 
 public class CoreUtils {
-    public static void handleBlockState(BlockState blockState, Consumer<BlockState> handler) {
+    public static <T extends Comparable<T>> void handleBlockState(BlockState blockState, Consumer<BlockState> handler) {
         for (Map.Entry<TagKey<Block>, Block> entry : EntryCache.TagMapCache.entrySet()) {
             if (blockState.is(entry.getKey()) && !blockState.is(entry.getValue())) {
-                handler.accept(entry.getValue().defaultBlockState());
+                BlockState result = entry.getValue().defaultBlockState();
+                blockState.getValues().forEach((property, comparable) -> {
+                    result.trySetValue((Property<T>) property, (T) comparable);
+                });
+                handler.accept(result);
+                return;
             }
         }
         Optional.ofNullable(EntryCache.UnitMapCache.get(blockState.getBlock()))
                 .filter(wrapper -> !blockState.is(wrapper))
                 .ifPresent(wrapper -> {
-                    handler.accept(wrapper.defaultBlockState());
+                    BlockState result = wrapper.defaultBlockState();
+                    blockState.getValues().forEach((property, comparable) -> {
+                        result.trySetValue((Property<T>) property, (T) comparable);
+                    });
+                    handler.accept(result);
                 });
     }
 }
